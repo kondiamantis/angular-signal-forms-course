ee4faa99520621d8c8f437a44afc1573738f5233
vasco <jhades.dev@gmail.com>
2026-06-16 15:00:19 +0200

    Angular Forms In Depth (Signals Edition) - recordings ongoing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 import { email, form, FormField, FormRoot, minLength, required } from '@angular/forms/signals';
 import { FieldErrorComponent } from '../field-error/field-error.component';
 import { LOGIN_FORM_DEFAULT, LoginData } from './login.model';
@@ -9,7 +10,12 @@ import { ResetIconComponent, SignInIconComponent } from './login-icons';
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [FormField, FormRoot, FieldErrorComponent, LoginLogoComponent, SignInIconComponent, ResetIconComponent],
+  imports: [FormField, FormRoot, FieldErrorComponent, LoginLogoComponent, SignInIconComponent, ResetIconComponent, JsonPipe],
 })
 export class LoginComponent {
+
+  loginModel = signal({...LOGIN_FORM_DEFAULT});
+
+  form = form(this.loginModel)
+
 }
--- src/app/login/login.model.ts
+++ src/app/login/login.model.ts
@@ -1,6 +1,11 @@
+
+
 export type LoginData = {
   email: string;
   password: string;
 }
 
-export const LOGIN_FORM_DEFAULT: LoginData = { email: '', password: '' };
+export const LOGIN_FORM_DEFAULT: LoginData = {
+  email: '',
+  password: ''
+};
