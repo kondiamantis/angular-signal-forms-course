3e35c4b646ce5aad19336f65cfd722400cfb754c
vasco <jhades.dev@gmail.com>
2026-06-22 18:06:24 +0200

    Angular Forms In Depth (Signals Edition) - recordings ongoing

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,20 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { applyEach, form, FormField, required } from '@angular/forms/signals';
+import { applyEach, form, FormField, FormRoot, required } from '@angular/forms/signals';
+import { FieldErrorComponent } from '../../field-error/field-error.component';
 import { Lesson, Step3Data } from './step3.model';
 
 @Component({
   selector: 'create-course-step-3',
   templateUrl: 'create-course-step-3.component.html',
   styleUrls: ['create-course-step-3.component.scss'],
-  imports: [FormField],
+  imports: [FormField, FormRoot, FieldErrorComponent],
 })
 export class CreateCourseStep3Component {
   step3Model = signal<Step3Data>({ lessons: [] });
 
+  step3Form = form(this.step3Model, (schema) => {
+    applyEach(schema.lessons, (lessonPath) => {
+      required(lessonPath.title, { message: 'Lesson title is required.' });
+      required(lessonPath.level, { message: 'Lesson level is required.' });
+    });
+  });
+
   addLesson() {
     this.step3Model.update((m) => ({
       ...m,
-      lessons: [...m.lessons, { title: '', level: 'beginner' } satisfies Lesson],
+      lessons: [...m.lessons, { title: '', level: '' } satisfies Lesson],
     }));
   }
 
