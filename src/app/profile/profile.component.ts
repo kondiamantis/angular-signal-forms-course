ba88a62fc249a6e7a1848785749e66bf4647e294
vasco <jhades.dev@gmail.com>
2026-06-23 14:01:09 +0200

    Angular Forms In Depth (Signals Edition) - recordings ongoing

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 import { email, form, FormField, FormRoot, readonly, required } from '@angular/forms/signals';
 import { AddressFormComponent } from '../address-form/address-form.component';
 import { FileUploadComponent } from '../file-upload/file-upload.component';
@@ -9,9 +10,16 @@ import { ProfileAvatarIconComponent, SaveIconComponent } from './profile-icons';
   selector: 'profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
-  imports: [FormField, FormRoot, AddressFormComponent, FileUploadComponent, ProfileAvatarIconComponent, SaveIconComponent],
+  imports: [FormField, FormRoot, AddressFormComponent, FileUploadComponent, ProfileAvatarIconComponent, SaveIconComponent, JsonPipe],
 })
 export class ProfileComponent {
 
+  profileModel = signal<ProfileData>({ ...PROFILE_DEFAULT });
+
+  profileForm = form(this.profileModel, (schema) => {
+    required(schema.email, { message: 'Email is required.' });
+    email(schema.email, { message: 'Enter a valid email address.' });
+    readonly(schema.email);
+  });
 
 }
