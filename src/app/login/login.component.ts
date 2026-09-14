45f9c836dcaf51778748f476f08341f2507a0b39
kdiamantis <kdiamantis@iknowhow.com>
2026-09-14 13:19:46 +0300

    connected login form with inputs in login html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { email, form, FormField, FormRoot, minLength, required } from '@angular/forms/signals';
 import { FieldErrorComponent } from '../field-error/field-error.component';
 import { LOGIN_FORM_DEFAULT, LoginData } from './login.model';
@@ -12,4 +12,14 @@ import { ResetIconComponent, SignInIconComponent } from './login-icons';
   imports: [FormField, FormRoot, FieldErrorComponent, LoginLogoComponent, SignInIconComponent, ResetIconComponent],
 })
 export class LoginComponent {
+
+  loginModel = signal({...LOGIN_FORM_DEFAULT})
+
+  form = form(this.loginModel)
+
+  constructor() {
+    effect(() => {
+      console.log(this.loginModel())
+    })
+  }
 }
