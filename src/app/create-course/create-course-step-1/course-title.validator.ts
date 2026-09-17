21cd9449b22a38b3a6af2e3f0cad58faa8519156
kdiamantis <kdiamantis@iknowhow.com>
2026-09-17 12:09:46 +0300

    added validator for step course 1 - wired form with input fields - added field error in inputs

--- src/app/create-course/create-course-step-1/course-title.validator.ts
+++ src/app/create-course/create-course-step-1/course-title.validator.ts
@@ -3,4 +3,15 @@ import { CourseListResponse } from './step1.model';
 
 export function courseTitleExists(path: SchemaPath<string>) {
 
+    validateHttp<string, CourseListResponse>(path, {
+        request: (ctx) => (ctx.value() ? '/api/courses' : undefined),
+        onSuccess: (result, ctx) => {
+            const found = result?.payload?.find(
+                (c) => c.description.toLowerCase() === ctx.value()?.toLowerCase()
+            )
+            return found ? { kind:'titleExists', message: 'Course title already exists.' } : null;
+        },
+        onError: () => null,
+    })
+
 }
